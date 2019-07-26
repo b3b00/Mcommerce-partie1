4ecab7fe593a26b98e746aee75e6e0efc4aa63c9
Olivier Duhart <olivier.duhart@sage.com>
2019-07-26 15:51:03 +0200

    thymeleaf ajout/suppression (link/forms)

--- src/main/java/com/ecommerce/microcommerce/web/controller/MainController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/MainController.java
@@ -8,6 +8,10 @@ import com.ecommerce.microcommerce.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,11 +22,27 @@ public class MainController {
     @Autowired
     ProductDao productDao;
 
-    @GetMapping(value = "/index")
-    public String Index(Model model,@RequestParam(name="title",required = false) String title ) {
+    private String RefreshIndex(Model model) {
         List<Product> products = productDao.findAll();
         model.addAttribute("products", products);
-        model.addAttribute("title",title);
+        model.addAttribute("prod", new Product());
         return "index";
     }
+
+    @GetMapping(value = "/index")
+    public String Index(Model model) {
+        return RefreshIndex(model);
+    }
+
+    @GetMapping(value = "/delete/{pid}")
+    public String Index(Model model,@PathVariable(name="pid",required = true) String pid ) {
+        productDao.delete(Integer.parseInt(pid));
+        return RefreshIndex(model);
+    }
+    @PostMapping(value="/add") 
+    public String Add(Model model,@ModelAttribute Product product) {
+        productDao.save(product);
+        return RefreshIndex(model);
+    }
+
 }
