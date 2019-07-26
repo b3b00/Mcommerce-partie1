c2903d7e52afd13fa911da87c811a13e6ca2285d
Olivier Duhart <olivier.duhart@sage.com>
2019-07-26 16:44:16 +0200

    fix

--- src/main/java/com/ecommerce/microcommerce/model/GitHubEvent.java
+++ src/main/java/com/ecommerce/microcommerce/model/GitHubEvent.java
@@ -4,6 +4,7 @@ import org.hibernate.validator.constraints.Length;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
