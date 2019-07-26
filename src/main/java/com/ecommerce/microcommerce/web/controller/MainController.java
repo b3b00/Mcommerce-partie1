cf4ae8537ed4c89e770d98d1cc6a10881e3c8fb7
Olivier Duhart <olivier.duhart@sage.com>
2019-07-26 13:49:58 +0200

    thymeleaf

--- src/main/java/com/ecommerce/microcommerce/web/controller/MainController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/MainController.java
@@ -1,17 +1,27 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.util.List;
+
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+
+    @Autowired
+    ProductDao productDao;
+
     @GetMapping(value = "/index")
     public String Index(Model model,@RequestParam(name="title",required = false) String title ) {
+        List<Product> products = productDao.findAll();
+        model.addAttribute("products", products);
         model.addAttribute("title",title);
         return "index";
     }
